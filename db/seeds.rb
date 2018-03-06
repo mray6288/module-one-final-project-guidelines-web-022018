76d2d373ec08254f91d3940e307b5a77804feafa
shirleyz9402 <shirleyz9402@yahoo.com>
2018-03-06 17:43:48 -0500

    meh

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,9 @@ require 'csv'
 CSV.foreach("./players_stats.csv", {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
   # binding.pry
   stat = PlayersStat.create(row.to_h)
-  player = Player.find_or_create_by(full_name: stat.name)
+  player = Player.where(full_name: stat.name, team: stat.team).first_or_create
   game = Game.find_or_create_by(id: stat.game_number)
+  game.update(home_team: stat.team , away_team: stat.opponent , date: stat.date)
   stat.game = game
   stat.player = player
   game.players_stats << stat
