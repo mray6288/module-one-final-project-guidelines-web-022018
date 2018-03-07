8e42beb74a69edbec5d2e46a6d2f97921d1530ea
shirleyz9402 <shirleyz9402@yahoo.com>
2018-03-07 17:57:58 -0500

    .

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ActiveRecord::Base
   has_many :players_stats
-  has_many :players, through: :players_statsrs
+  has_many :players, through: :players_stats
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,4 @@
 class Player < ActiveRecord::Base
   has_many :players_stats
   has_many :games, through: :players_stats
-  def find_game
-  end
-  def evaluate_performance
-  end
 end
