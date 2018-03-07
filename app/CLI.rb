2283c911fb3458f770e75f44c9390d47bdb1515f
Ray Blasko <mray6288@gmail.com>
2018-03-07 12:57:57 -0500

    did a whole bunch of stuff and forgot about committing

---
+++ app/CLI.rb
@@ -0,0 +1,135 @@
+def greeting
+	puts "\nWelcome to NBA stats interface"
+end
+
+def display_stats(player_stats, stat)
+	proj = ('projected_' + stat)
+	act = ('actual_' + stat)
+	puts "\n opponent | #{proj} | #{act}"
+	player_stats.each {|row| puts "    #{row.opponent}   |       #{row.send(proj)}       |     #{row.send(act)}"}
+	
+end
+
+
+def prompt_player_name
+	puts "\nEnter player name:"
+	puts '(type help for player list)'
+	name = gets.chomp.split(' ').map(&:capitalize).join(' ')
+	if name == 'Help'
+		puts '', Player.distinct.pluck(:full_name).sort
+		puts "\n Enter player name:"
+		name = gets.chomp.split(' ').map(&:capitalize).join(' ')
+	end
+	player = Player.find_by(full_name: name)
+	until player
+		puts 'INVALID PLAYER'
+		puts "\nEnter player name:"
+		puts '(type help for player list)'
+		name = gets.chomp.split(' ').map(&:capitalize).join(' ')
+		player = Player.find_by(full_name: name)
+	end
+	PlayersStat.where(player_id: player.id)
+end
+
+def display_past_stats(player_stats, stat)
+	proj = ('projected_' + stat)
+	act = ('actual_' + stat)
+	puts "\n opponent | #{proj} | #{act}"
+	total_proj = 0.0
+	total_act = 0.0
+	count = 0.0
+	player_stats.each do |row|
+		puts "    #{row.opponent}   |      #{row.send(proj)}      |     #{row.send(act)}"
+		count += 1
+		total_proj += row.send(proj)
+		total_act += row.send(proj)
+	end
+	avg_proj = total_proj/count
+	avg_act = total_act/count
+	puts "---------------------------------------------"
+	puts "    AVG   |     #{avg_proj.round(1)}      |     #{avg_act.round(1)}"
+	
+end
+
+def display_proj_stats(player_stats, stat)
+
+end
+
+def prompt_team_name(opponent=false)
+	puts "\nEnter team name:"
+	puts '(type help for team list)'
+	team = gets.chomp.upcase
+	team_names = PlayersStat.distinct.pluck(:team).sort
+	if team == 'HELP'
+		puts '', team_names
+		puts "\nEnter team name:"
+		team = gets.chomp.upcase
+	end
+	until team_names.include?(team)
+		puts 'INVALID TEAM'
+		puts "\nEnter team name:"
+		puts '(type help for team list)'
+		team = gets.chomp.upcase
+	end
+	if opponent
+		PlayersStat.where(opponent: team)
+	else
+		PlayersStat.where(team: team)
+	end
+
+end
+
+def prompt_stat
+	puts "\nEnter stat to get:"
+	puts '(type help for stat list)'
+	stat = gets.chomp.downcase
+	columns = PlayersStat.column_names.map {|col| col.split('_')[-1] if col.include?('projected')}
+	if stat == 'help'
+		puts ''
+		puts columns
+		puts "\nEnter stat to get:"
+		stat = gets.chomp.downcase
+	end
+	until columns.include?(stat)
+		puts 'INVALID STAT'
+		puts "\nEnter stat to get:"
+		puts '(type help for stat list)'
+		stat = gets.chomp.downcase
+	end
+	stat
+end
+
+def select_option
+	puts "\nSelect option:"
+	puts '1 -> player stat, 2 -> team stat, 3 -> opponent stat 4 -> quit'
+
+	input = gets.chomp.to_i
+	until (1..4) === input
+		puts 'INVALID OPTION'
+		puts 'Select option:'
+		puts '1 -> player stat, 2 -> team stat, 3 -> opponent stat 4 -> quit'
+		input = gets.chomp.to_i
+	end
+	input
+end
+
+def start_interface
+	greeting
+	opt = select_option
+	until opt == 4
+		if opt == 1
+			player_stats = prompt_player_name
+			stat = prompt_stat
+			display_past_stats(player_stats, stat)
+		elsif opt == 2
+			team_stats = prompt_team_name
+			stat = prompt_stat
+			display_past_stats(team_stats.select("opponent, sum(projected_#{stat}) as projected_#{stat}, sum(actual_#{stat}) as actual_#{stat}").group(:game_id), stat)
+		elsif opt == 3
+			team_stats = prompt_team_name(opponent=true)
+			stat = prompt_stat
+			display_past_stats(team_stats.select("team as opponent, sum(projected_#{stat}) as projected_#{stat}, sum(actual_#{stat}) as actual_#{stat}").group(:game_id), stat)
+		end
+		opt = select_option
+	end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
 require 'csv'
 
+
+
 CSV.foreach("./players_stats.csv", {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
   # binding.pry
   stat = PlayersStat.create(row.to_h)
   player = Player.where(full_name: stat.name, team: stat.team).first_or_create
   game = Game.find_or_create_by(id: stat.game_number)
-  game.update(home_team: stat.team , away_team: stat.opponent , date: stat.date)
+  if !game.home_team
+  	game.update(home_team: stat.team , away_team: stat.opponent , date: stat.date)
+  end
   stat.game = game
   stat.player = player
   game.players_stats << stat
